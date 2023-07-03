18b6a2999fa3521b0ef70cd6dbcc50e1397ad4ca
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2023-07-03 18:47:16 +0700

    thanhngan

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Login from './pages/Login';
 // import HomePage from './pages/HomePage';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <Login />
+    <Register/>
+    // <Login />
     // <HomePage />
   );
 }
