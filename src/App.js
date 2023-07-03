89291ca5717e20f31e04c052d3e3f1a9650c05f9
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2023-07-03 18:55:44 +0700

    thanhngan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-import Login from './pages/Login';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
 // import HomePage from './pages/HomePage';
-import Register from './pages/Register';
+import Register from "./pages/Register";
 
 function App() {
+  // đang cấu hinh router
+  const router = createBrowserRouter([{ path: "", element: "" }]);
   return (
-    <Register/>
+    <Register />
     // <Login />
     // <HomePage />
   );
