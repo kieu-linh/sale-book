3c8c80dca24d88984b65b4a79f1dbb656d4273a5
Kieu Linh <hothikieulinh1407@gmail.com>
2023-07-17 09:50:52 +0700

    linh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+<<<<<<< HEAD
 import HomePage from './pages/HomePage';
 // import HomePage from './pages/HomePage';
 import Register from "./pages/Register";
@@ -13,6 +13,23 @@ function App() {
     // <Register />
     // <Login />
     <HomePage />
+=======
+// import HomePage from './pages/HomePage';
+import Register from "./pages/Register";
+import Home2 from "./pages/HomePage2";
+
+function App() {
+  return (
+    <> 
+    {/* <Register /> */}
+    <Home2/>
+    </>
+   
+    
+    // <Login />
+    // <HomePage />
+  
+>>>>>>> 39b9ba42dcf66b992a9130cc53899c542c0c0de6
   );
 }
 
